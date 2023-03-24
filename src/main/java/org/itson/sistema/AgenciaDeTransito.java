69db4ebc7e7e2eb828af2a49552b78db80bcdf7d
Daniel Peña <daniel.pena229185@potros.itson.edu.mx>
2023-03-24 12:32:30 -0700

    Proyecto de NetBeans con Maven, con dependencias incluidas de Jasper Reports y JDBC. El jar del conector con Mysql está en la carpeta de drivers.

---
+++ src/main/java/org/itson/sistema/AgenciaDeTransito.java
@@ -0,0 +1,13 @@
+package org.itson.sistema;
+
+/**
+ *
+ * @author Daniel Armando Peña Garcia ID:229185
+ * @author 
+ */
+public class AgenciaDeTransito {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
